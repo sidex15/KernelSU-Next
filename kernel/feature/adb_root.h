3c59f5b16f10b1c34f1a5256858ff1790064372e
cvnertnc <148134890+cvnertnc@users.noreply.github.com>
2026-07-08 16:10:23 +0300

    kernel: this file does not exist
    Signed-off-by: cvnertnc <148134890+cvnertnc@users.noreply.github.com>

--- kernel/feature/adb_root.h
+++ kernel/feature/adb_root.h
@@ -1,7 +1,6 @@
 #ifndef __KSU_H_ADB_ROOT
 #define __KSU_H_ADB_ROOT
 #include <asm/ptrace.h>
-#include "runtime/user_arg_ptr.h"
 
 long ksu_adb_root_handle_execve(const char *filename, struct user_arg_ptr *envp_p);
 
